2865f4403571a47f23d551bd05438ffe7d598959
Jakob Miksch <jakob@meggsimum.de>
2022-09-13 17:34:03 +0200

    Update zonal_stats.sh

--- process_scripts/zonal_stats.sh
+++ process_scripts/zonal_stats.sh
@@ -2,7 +2,7 @@
 
 # Script to compute zonal statistics of a raster file cropped by an provided GeoJSON polygon.
 # Usage: zonal_stats.sh "$(<demo.geojson)"
-# Input: The content of a GeoJSON polygon to as first arguement
+# Input: The content of a GeoJSON polygon to as first argument
 # Returns: An JSON to standard out with 'max', 'mean', 'min', 'stddev', 'valid percent'
 
 # hide warnings see https://gis.stackexchange.com/a/358593
